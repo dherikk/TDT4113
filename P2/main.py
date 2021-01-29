3b68b2a5708eed19e2c3622d57f7c7ffe8dbb05e
D <dherikjd@stud.ntnu.no>
2021-01-29 22:17:46 +0100

    Done with Historian class

--- P2/main.py
+++ P2/main.py
@@ -1,5 +1,4 @@
 import random as r
-from itertools import groupby as group
 
 class Player:
 
@@ -8,13 +7,13 @@ class Player:
 
     def __init__(self):
         self.id = counter
-        counter += 1
+        Player.counter += 1
         Player.move_history[self.id] = []
     def select_action(self, action):
         return action
 
     def set_opponent(self, opponent):
-        self.opponent = opponent
+        self.opponentid = opponent
 
     def recieve_result(self):
         return None
@@ -33,11 +32,16 @@ class Action:
     def __eq__(self, value):
         return self.action == value
 
+    def __str__(self):
+        return str(self.value)
+
+    
+
 class Random(Player):
     def __init__(self):
         super().__init__()
 
-    def select_action(self, action):
+    def select_action(self):
         return super().select_action(Action(r.randint(0,2)))
 
 class Sequential(Player):
@@ -45,7 +49,7 @@ class Sequential(Player):
         super().__init__()
         self.current = 0
 
-    def select_action(self, action):
+    def select_action(self):
         self.counter += 1
         return super().select_action(Action(self.counter % 3))
 
@@ -53,8 +57,8 @@ class MostCommon(Player):
     def __init__(self):
         super().__init__()
 
-    def select_action(self, action):
-        tmp_1 = Player.move_history[self.opponent-1]
+    def select_action(self):
+        tmp_1 = Player.move_history[self.opponentid]
         return super().select_action(Action(max(set(tmp_1), key = tmp_1.count)))
 
 class Historian(Player):
@@ -62,17 +66,44 @@ class Historian(Player):
         self.depth = depth
         super().__init__()
 
-    def select_action(self, action):
-        tmp_1 = []
+    def select_action(self):
         try:
-            tmp1_1 = Player.move_history[self.opponent-1][-2:]
+            tmp_1 = Player.move_history[self.opponentid]
+            tmp_2 = tmp_1[self.depth * -1:]
+            tmp_3 = [(i+len(tmp_2)) for i in range(len(tmp_1)-self.depth) if tmp_1[i:i+len(tmp_2)] == tmp_2]
+            tmp_4 = [tmp_1[i] for i in tmp_3]
+            return super().select_action(Action(max(set(tmp_4), key = tmp_4.count)))
         except:
-            tmp_1 = [r.randint(0,2), r.randint(0,2)]
-        return super().select_action(action)
+            return super().select_action(Action(r.randint(0,2)))
+    
+class SingleGame:
+
+    def __init__(self, player1, player2):        
+        self.player1.set_opponent(player2.id)
+        self.player2.set_opponent(player1.id)
+        self.p1_points = 0
+        self.p2_points = 0
+    
+    def perform_game(self):
+        tmp_1 = self.player1.select_action()
+        tmp_2 = self.player2.select_action()
+
+        if tmp_1 > tmp_2:
+            self.p1_points += 1
+        elif tmp_1 == tmp_2:
+            self.p1_points += 0.5
+            self.p2_points += 0.5
+        else:
+            self.p2_points += 1
+
+# Husk å endre action-klassen fra value til action
+        
+
 
 
 def main():
-   """main"""
+    print('test')
+    """main"""
    
 if __name__ == "__main__":
     main()
