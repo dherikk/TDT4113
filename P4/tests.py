1b3ca6731249fcd6782e0c759a4fec6d92718f88
Dherik <jendeva@hotmail.com>
2021-02-24 01:05:39 +0100

    Pylint tests suggestions

--- P4/tests.py
+++ P4/tests.py
@@ -1,6 +1,8 @@
+"""Tests for P4 main"""
 import main as m
 
-def test_Stack():
+def test_stack():
+    """tests stack data struct"""
     stack = m.Stack()
     stacklist = [i for i in range(15)]
     for e in stacklist:
@@ -11,24 +13,30 @@ def test_Stack():
         assert last == i
     assert stack.isEmpty() == True
 
-def test_Queue():
+
+def test_queue():
+    """tests queue data struct"""
     queue = m.Queue()
     queuelist = [i for i in range(15)]
     for e in queuelist:
         queue.push(e)
-    assert queue.isEmpty() == False
+    assert not queue.isEmpty()
     for i in range(15):
         last = queue.pop()
         assert last == i
-    assert queue.isEmpty() == True
+    assert queue.isEmpty()
 
-def test_RPN():
+
+def test_rpn():
+    """tests rpn method"""
     calc = m.Calculator()
     calc.output_queue.push(7)
     calc.output_queue.push(calc.functions['EXP'])
     assert round(calc.calculate_rpn(), 6) == round(m.np.exp(7), 6)
-    
-def test_calculate_to_RPN():
+
+
+def test_calculate_to_rpn():
+    """tests calculate_to_rpn"""
     calc = m.Calculator()
     calc.calculate_to_rpn([
         calc.functions['EXP'],
@@ -39,7 +47,7 @@ def test_calculate_to_RPN():
         calc.operators['ADD'],
         1,
         ')'
-        ])
+    ])
 
     temp_l = calc.output_queue._items
     global temp_l2
@@ -54,13 +62,17 @@ def test_calculate_to_RPN():
     for i in temp_l:
         assert isinstance(i, temp_l2[temp_l.index(i)])
 
+
 def test_text_parse():
+    """tests text_parse"""
     calc = m.Calculator()
     temp_l3 = calc.text_parse("2 2 add 2 multiply sin")
     for i in temp_l3:
         assert isinstance(i, temp_l2[temp_l3.index(i)])
 
+
 def test_all():
+    """Tests all"""
     calc = m.Calculator()
     test = "((15 DIVIDE (7 SUBTRACT (1 ADD 1))) MULTIPLY 3) SUBTRACT (2 ADD (1 ADD 1))"
     test2 = "15 ADD 700 ADD 7"
@@ -69,13 +81,16 @@ def test_all():
     assert int(var1) == 5
     assert int(var2) == 722
 
+
 def main():
-    test_Stack()
-    test_Queue()
-    test_RPN()
-    test_calculate_to_RPN()
+    """main method"""
+    test_stack()
+    test_queue()
+    test_rpn()
+    test_calculate_to_rpn()
     test_text_parse()
     test_all()
+
 
 if __name__ == '__main__':
     main()
